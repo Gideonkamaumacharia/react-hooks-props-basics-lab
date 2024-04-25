c5c7179b19c69bd2b2b10eadd9707f94e9797d24
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-25 07:39:02 +0300

    create Link

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Links from ".components/links"
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && <p>{props.bio}</p>}
+     
+      {props.bio && <p>{props.bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
       <Links github={props.links.github} linkedin={props.links.linkedin} />
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function Links(props) {
-  return (
+function Links(props){
     <div>
-      <h3>Links</h3>
-      <a href={props.github}>{props.github}</a>
-      <a href={props.linkedin}>{props.linkedin}</a>
+        <h3>Links</h3>
+        <a href={props.github}>{props.github}</a>
+        <a href={props.linkedin}>{props.linkedin}</a>
     </div>
-  );
 }
+export default Links
