28659c9b8ca179700e2392ac17a07369a3e1fbc8
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-25 08:06:59 +0300

    modify About

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Links from ".components/links"
+import Links from "./Links"
 
 function About(props) {
   return (
@@ -9,9 +9,10 @@ function About(props) {
       {props.bio && <p>{props.bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
-      <Links github={props.links.github} linkedin={props.links.linkedin} />
+      {<Links github={props.github} linkedin={props.linkedin} />}
     </div>
   );
 }
 
 export default About;
+
